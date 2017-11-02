00f32d05740f173c7cda401f0635d1c6f9eea8d7
Naphorn <naphorn.chok@gmail.com>
2017-11-03 01:58:42 +0700

    Operators and Expressions

---
+++ Part03/Part03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Part03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 17;
+            double y = x / 4.0;
+            double z = y * y;
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            int a = 5;
+            int b = 4;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + (b++));
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + (++b));
+            Console.WriteLine(a + b);
+            Console.WriteLine(14 / a);
+            Console.WriteLine(14 % a);
+
+        }
+    }
+}
