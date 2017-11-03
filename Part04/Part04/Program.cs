425ae8174ed6e10bd5df8ef2286aed9b1b8ec3a3
Naphorn <naphorn.chok@gmail.com>
2017-11-04 01:26:04 +0700

    ToString()

---
+++ Part04/Part04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Part04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            string myString = a.ToString();
+            Console.WriteLine(myString);
+        }
+    }
+}
