54a92412c4342934411a2e0d073703523a25eaa8
Naphorn <naphorn.chok@gmail.com>
2017-11-04 20:27:48 +0700

    Test 4

--- Part05/Part05/Program.cs
+++ Part05/Part05/Program.cs
@@ -5,21 +5,34 @@ namespace Part05
     {
         static void Main(string[] args)
         {
-            Compare();
-            Console.ReadKey();
+            try
+            {
+                Console.Write("Please enter width : ");
+                double w = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Please enter length : ");
+                double l = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Please enter height : ");
+                double h = Convert.ToDouble(Console.ReadLine());
+
+                Area(w, l, h);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
 
-        static void Compare()
+        static void Area(double w, double l, double h)
         {
-            byte dec = 16;
-
-            for (int i = 1; i <= 100; i++)
-            {
-                if ((i & dec) == dec)
-                {
-                    Console.Write(i + " ");
-                }
-            }
+            double a = ((w + l) * h) * 1 / 2;
+            Console.WriteLine("Result = {0} cm^2", a);
         }
     }
 }
